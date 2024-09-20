cb4a82457409a3c9807b62784958893e5d1f0d5d
Lucas Paes <paesleme1996@gmail.com>
2024-09-19 21:53:19 -0300

    algumas modificaçoes

--- Assets/JavaScript/typing.js
+++ Assets/JavaScript/typing.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
     new TypeIt(".second-text", {
         speed: 150,
         loop: true
-    }).type('Desenvolvedor web.', {delay: 900}).delete(18).type('FreeLancer.', {delay: 900})
+    }).type('Desenvolvedor web.', {delay: 900}).delete(18).type('FreeLancer.', {delay: 900}).delete(11)
+    .type('Gamer.',{delay: 900}).delete(6).type('Estudante.',{delay: 900})
 
     .go()
 })
